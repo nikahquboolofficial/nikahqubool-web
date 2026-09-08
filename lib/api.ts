38c1c5d8640458d7ea8c74fb6825b13b95db6428
Yasar Mishkat <yasarmishkat5@gmail.com>
2026-09-08 15:15:56 +0530

    updated homepage

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://localhost:7204/api";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://api.nikahqubool.in/api";
 
 // ⚡ Dynamic SignalR Hub URL
 export const SIGNALR_HUB_URL = API_BASE_URL.replace(/\/api\/?$/, '') + "/chatHub";
